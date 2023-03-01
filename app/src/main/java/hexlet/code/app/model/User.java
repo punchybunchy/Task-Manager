a0f05415107eb2309ff459432157e815a4bb505a
tyoma <artyomlosev.work@gmail.com>
2023-03-01 14:43:39 +0600

    Add User.java

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,57 @@
+package hexlet.code.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import static javax.persistence.GenerationType.AUTO;
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    @Temporal(TIMESTAMP)
+    private Date createdAt;
+
+    public User(final Long id) {
+        this.id = id;
+    }
+
+}
